f384a475f3da0db8d8a3c020230e8edaeef4e9cf
Sam Soffes <sam@samsoff.es>
2009-02-28 15:15:59 -0600

    Updated styles more

---
+++ application/views/_templates/blog.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+<meta http-equiv="Content-Style-Type" content="text/css" />
+<?php echo $head ?>
+</head>
+
+<body>
+<div id="wrapper">
+<?php echo $content ?>
+</div>
+<div id="footer">
+	<p>Copyright <?php echo html::anchor(FALSE, 'Sam Soffes') ?> 2005-2009. <a href="http://github.com/samsoffes/samsoff.es/">View source on GitHub</a>.</p>
+</div>
+</body>
+</html>

1b5f6d261e3fe24df0bf0e3bba504a11d1d5e798
Sam Soffes <sam@samsoff.es>
2009-03-02 02:43:17 -0600

    Updated config file to go live

--- application/config/config.php
+++ application/config/config.php
@@ -4,7 +4,7 @@
  * then a full URL will be used, eg: http://localhost/kohana/. If it only includes
  * the path, and a site_protocol is specified, the domain will be auto-detected.
  */
-$config['site_domain'] = 'new.samsoff.es';
+$config['site_domain'] = IN_PRODUCTION ? 'samsoff.es' : 'dev.samsoff.es';
 
 /**
  * Force a default protocol to be used by the site. If no site_protocol is

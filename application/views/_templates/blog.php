e14cf8b9c4a8a4f619fe6b7434f4d987272c1ef4
Sam Soffes <sam@samsoff.es>
2009-03-02 00:38:59 -0600

    Finished ajax comments

--- application/views/_templates/blog.php
+++ application/views/_templates/blog.php
@@ -14,4 +14,8 @@
 	<p>Copyright <?php echo html::anchor(FALSE, 'Sam Soffes') ?> 2005-2009. <a href="http://github.com/samsoffes/samsoff.es/">View source on GitHub</a>.</p>
 </div>
 </body>
+<?php if (IN_PRODUCTION) : ?>
+<script type="text/javascript">document.write(unescape("%3Cscript src='http://www.google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));</script>
+<script type="text/javascript">try {var pageTracker = _gat._getTracker("UA-5609322-1");pageTracker._trackPageview();} catch(err) {}</script>
+<?php endif ?>
 </html>
--- html/js/comments.js
+++ html/js/comments.js
@@ -2,22 +2,32 @@ $(function() {
 	$('form#comment-form').submit(function(e) {
 		e.preventDefault();
 		
+		// that = this
 		var form_el = this;
 		var form = $(form_el);
 
 		// Disabled fields
-		var inputs = $('input[type=text], textarea', form_el);
+		var inputs = $('input, textarea', form_el);
 		inputs.attr('disabled', 'disabled');
 		inputs.css('color', '#999');
 
-		// Hide the submit button
-		$('input[type=submit]').hide();
-		
-		form.append('<div class="loading">Posting comment...</div>');
+		// Show indicator
+		$('p:last', form_el).append('<span class="loading">Posting comment...</span>');
+
+		// Setup data to post
+		var postData = {
+			'name': $('input#comment-name', form_el).val(),
+			'email': $('input#comment-email', form_el).val(),
+			'website': $('input#comment-website', form_el).val(),
+			'content': $('textarea#comment-content', form_el).val(),
+		};
 
 		// Post the comment
-		$.post(form_el.action, form.serialize(), function(data, textStatus) {
-			
+		$.post(form_el.action, postData, function(data, textStatus) {
+
+			// Eval JSON string
+			data = $.evalJSON(data);
+
 			// Posted successfully
 			if (data.success == 'true') {
 
@@ -25,14 +35,25 @@ $(function() {
 				resetForm(form_el, true);
 				
 				// Add their comment to the list
+				$('div.comments ul').append(data.html);
+				
+				// Scroll to it
+				//$('html,body').animate({scrollTop: $('div.comments ul li:last').offset().top - 50}, 300);
 			}
 			
 			// There was an error
 			else {
-			
+
 				// Reset the form, but dont clear it
 				resetForm(form_el, false);
-			}		
+				
+				// Loop through errors
+				for (var key in data.errors) {			
+						
+					// Use the style attr, so we can clear it later
+					$('#comment-'+key).attr('style', 'border-color:#ff5b50;border-width:2px;margin:0;');
+				}
+			}
 		});
 	});
 });
@@ -40,15 +61,14 @@ $(function() {
 
 function resetForm(form_el, clear) {
 
-	$('div.loading', form_el).remove();
+	$('span.loading', form_el).remove();
 
-	var inputs = $('input[type=text], textarea', form_el);
+	var inputs = $('input, textarea', form_el);
+	inputs.removeAttr('style');
 	inputs.removeAttr('disabled');
 	inputs.css('color', '#000');
 
 	if (clear) {
-		inputs.val('');
+		$('input[type=text], textarea', form_el).val('');
 	}
-
-	$('input[type=submit]', form_el).show();
 }

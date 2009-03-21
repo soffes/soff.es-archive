3d2a5cd31c4aa4c077ed3cc21c7d3f03bfd352b1
Sam Soffes <sam@samsoff.es>
2009-03-21 14:54:15 -0500

    Added about.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
-	map.resources :posts,  :has_many => :comments
+	map.resources :posts, :has_many => :comments
+	map.music 'music', :controller => 'music'
+	map.about 'about', :controller => 'about'
 
-	map.connect ':controller/:action/:id'
+	map.connect ':controller/:action/:id', :controller => 'posts'
 	map.connect ':controller/:action/:id.:format'
 end

0d1141ad07bcfa47ea6c3866354068a1944ef29d
Sam Soffes <sam@samsoff.es>
2009-03-23 02:15:39 -0500

    jRails now includes its javascript files from Google instead of locally

--- vendor/plugins/jrails/init.rb
+++ vendor/plugins/jrails/init.rb
@@ -1,6 +1,6 @@
 # uncomment to use jQuery.noConflict()
 #ActionView::Helpers::PrototypeHelper::JQUERY_VAR = 'jQuery'
 
-ActionView::Helpers::AssetTagHelper::JAVASCRIPT_DEFAULT_SOURCES = ['jquery','jquery-ui','jrails']
+ActionView::Helpers::AssetTagHelper::JAVASCRIPT_DEFAULT_SOURCES = ['http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js', 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.1/jquery-ui.min.js', 'jrails']
 ActionView::Helpers::AssetTagHelper::reset_javascript_include_default
 require 'jrails'

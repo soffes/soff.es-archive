02efc7495a92f165f87a2f0f1d452b4ca43deac1
Sam Soffes <sam@samsoff.es>
2009-04-04 19:17:51 -0500

    Cleaned up pagination urls and added page caching

---
+++ app/sweepers/post_sweeper.rb
@@ -0,0 +1,16 @@
+class PostSweeper < ActionController::Caching::Sweeper
+  observe Post
+  
+  def after_save(post)
+    expire_cache(post)
+  end
+  
+  def after_destory(post)
+    expire_cache(post)
+  end
+  
+  def expire_cache(post)
+    expire_page :controller => 'posts', :action => 'page'
+  end
+end
+  

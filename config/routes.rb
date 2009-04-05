d3ecf2689b3fb337e225c725541794c6408dcec9
Sam Soffes <sam@samsoff.es>
2009-04-05 01:42:40 -0500

    Fixed feed

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 ActionController::Routing::Routes.draw do |map|
   map.home '', :controller => 'posts', :action => 'index'
 	map.resources :posts, :has_many => :comments
-	map.connect 'page/:page',
-    :controller => 'posts',
-    :action => 'index',
-    :requirements => { :page => /\d+/},
-    :page => nil
-	map.post_slug 'post/:slug', :controller => 'posts', :action => 'show'
+#	map.connect 'page/:page',
+#    :controller => 'posts',
+#    :action => 'index',
+#    :requirements => { :page => /\d+/},
+#    :page => nil
+#	map.post_slug 'post/:slug', :controller => 'posts', :action => 'show'
 	
 	# Static pages
 	map.music 'music', :controller => 'music'

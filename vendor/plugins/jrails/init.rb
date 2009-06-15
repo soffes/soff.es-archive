71379b6a5259663f4c77af225bf95dd3bc7de307
Sam Soffes <sam@samsoff.es>
2009-06-15 02:02:49 -0500

    Replaced jRails with jQuery

--- vendor/plugins/jrails/init.rb
+++
@@ -1,6 +0,0 @@
-# uncomment to use jQuery.noConflict()
-#ActionView::Helpers::PrototypeHelper::JQUERY_VAR = 'jQuery'
-
-ActionView::Helpers::AssetTagHelper::JAVASCRIPT_DEFAULT_SOURCES = ['http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js', 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.1/jquery-ui.min.js', 'jrails']
-ActionView::Helpers::AssetTagHelper::reset_javascript_include_default
-require 'jrails'

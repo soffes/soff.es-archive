8bc7143ca2efc543de9afdd2c546cfc646223df7
Sam Soffes <sam@samsoff.es>
2009-07-22 15:53:08 -0500

    Fixed feed issues

--- app/views/posts/index.atom.builder
+++ app/views/posts/index.atom.builder
@@ -5,7 +5,7 @@ atom_feed do |feed|
 	@posts.each do |post|
 		feed.entry(post) do |entry|
 			entry.title(post.title)
-			entry.content(post.body, :type => 'html')
+			entry.content(markdown(post.body), :type => 'html')
 			entry.author { |author| author.name("Sam Soffes") }
 		end
 	end
--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.3' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -10,9 +10,8 @@ Rails::Initializer.run do |config|
   # Gem dependencies
   config.gem 'mislav-will_paginate', :version => '~> 2.2.3', :lib => 'will_paginate', :source => 'http://gems.github.com'
   config.gem 'rtomayko-rdiscount', :version => '~> 1.3.4', :lib => 'rdiscount', :source => 'http://gems.github.com'
-  config.gem 'haml'
-  config.gem 'chriseppstein-compass', :version => '~> 0.6.15', :lib => 'compass', :source => 'http://gems.github.com'
-#  config.gem 'passenger'
+  config.gem 'haml', :version => '2.2.2'
+  config.gem 'chriseppstein-compass', :version => '~> 0.8.8', :lib => 'compass', :source => 'http://gems.github.com'
   
   # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
   config.time_zone = 'Central Time (US & Canada)'

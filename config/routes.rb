243a01524903cc93acaeaa1488fce6daf629012f
Sam Soffes <sam@samsoff.es>
2009-08-06 12:09:14 -0500

    Cleaned up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,9 @@
 ActionController::Routing::Routes.draw do |map|
-  map.home '', :controller => 'posts', :action => 'index'
 	map.resources :posts, :has_many => :comments
-#	map.connect 'page/:page',
-#    :controller => 'posts',
-#    :action => 'index',
-#    :requirements => { :page => /\d+/},
-#    :page => nil
 	map.post_slug 'post/:slug', :controller => 'posts', :action => 'show'
 	
-	# Static pages
 	map.music 'music', :controller => 'music'
 	map.about 'about', :controller => 'about'
 
-	#map.connect ':controller/:action/:id'
-	#map.connect ':controller/:action/:id.:format'
+  map.root :posts
 end

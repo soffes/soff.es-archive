20a963d07a6275f3f3ce59d039dd7cd432ab0697
Sam Soffes <sam@samsoff.es>
2009-08-20 16:15:00 -0500

    Started porting to Mootools. Unstable

--- config/initializers/asset_defaults.rb
+++ config/initializers/asset_defaults.rb
@@ -1,2 +1,4 @@
 ActionView::Helpers::AssetTagHelper.register_javascript_expansion :jquery => ["http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"]
 ActionView::Helpers::AssetTagHelper.register_javascript_expansion :jquery_ui => ["http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.2/jquery-ui.min.js"]
+ActionView::Helpers::AssetTagHelper.register_javascript_expansion :mootools => ["http://ajax.googleapis.com/ajax/libs/mootools/1.2.3/mootools-yui-compressed.js"]
+ActionView::Helpers::AssetTagHelper.register_javascript_expansion :mootools_more => ["mootools-1.2.3.1-more"]

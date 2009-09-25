37a47fd14677bd4d9ea84801d04740e5b2bd731e
samsoffes <sam@samsoff.es>
2009-09-25 13:49:44 -0500

    Updated

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,7 +19,7 @@ config.action_view.cache_template_loading            = true
 # config.cache_store = :mem_cache_store
 
 # Enable serving of images, stylesheets, and javascripts from an asset server
-# config.action_controller.asset_host = "http://assets.example.com"
+config.action_controller.asset_host = "http://asset%d.samsoff.es"
 
 # Disable delivery errors, bad email addresses will be ignored
 # config.action_mailer.raise_delivery_errors = false

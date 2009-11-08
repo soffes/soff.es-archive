815d3a2dde16ecbfd41ba134a0a0b7a24cd66c4d
Sam Soffes <sam@samsoff.es>
2009-11-08 16:39:42 -0600

    Updated home page and fought with cucumber

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,7 @@
 class HomeController < ApplicationController
+
+  def index
+    @recent_post = Post.find(:all, :limit => 1).first
+  end
+
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,7 @@
 class Category < ActiveRecord::Base
+
   attr_accessible :name, :permalink
+
   has_many :posts
+
 end

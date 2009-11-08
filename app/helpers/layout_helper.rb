6f52b586cd14bbe57c4c01b0500e4d4f190184d3
Sam Soffes <sam@samsoff.es>
2009-11-07 21:02:37 -0600

    Started on new design

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -22,13 +22,13 @@ module LayoutHelper
   end
   
   def feed_tag(title, url, type = "application/atom+xml")
-    "<link href=\"#{url}\" rel=\"alternate\" title=\"#{title}\" type=\"#{type}\" />\n"
+    "<link href=\"#{url}\" rel=\"alternate\" title=\"#{title}\" type=\"#{type}\" />"
   end
   
   def google_analytics(id, always = false)
     # By default, only return Google Analytics code if in production
     # This can be overridden but passing true for the second argument
-    (always || Rails.env.to_sym == :production) ? "<script type=\"text/javascript\">var gaJsHost=((\"https:\"==document.location.protocol)?\"https://ssl.\":\"http://www.\");document.write(unescape(\"%3Cscript src='\"+gaJsHost+\"google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E\"));</script><script type=\"text/javascript\">try{var pageTracker=_gat._getTracker(\"#{id}\");pageTracker._trackPageview();}catch(err){}</script>" : ""
+    "<script type=\"text/javascript\">var gaJsHost=((\"https:\"==document.location.protocol)?\"https://ssl.\":\"http://www.\");document.write(unescape(\"%3Cscript src='\"+gaJsHost+\"google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E\"));</script><script type=\"text/javascript\">try{var pageTracker=_gat._getTracker(\"#{id}\");pageTracker._trackPageview();}catch(err){}</script>" if (always || Rails.env.to_sym == :production)
   end
 
 end

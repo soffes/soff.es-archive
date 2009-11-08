7d95f2059fe66b2580cae3a4bf5b2c2f100d0830
Sam Soffes <sam@samsoff.es>
2009-11-07 20:55:07 -0600

    Generated static controller and got pagination working

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   
   def index
-    @posts = Post.all
+    @posts = Post.search(nil, params[:page])
   end
   
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 ActionController::Routing::Routes.draw do |map|
 
-  map.resources :comments
+  # Blog
   map.resources :posts
+  map.post_slug "post/:slug", :controller => "posts", :action => "show"
+  map.blog "blog", :controller => "posts"
+  map.resources :comments
   
-  map.root :controller => "posts"
+  # Static Pages
+  map.root :controller => "home"
+  map.music "music", :controller => "music"
+  map.about "about", :controller => "about"
 
 end

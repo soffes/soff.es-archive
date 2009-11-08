bd046633e426cba3fef40c28ffee56a202326f22
Sam Soffes <sam@samsoff.es>
2009-11-08 01:05:55 -0600

    Updated gem config

--- config/environment.rb
+++ config/environment.rb
@@ -15,10 +15,10 @@ Rails::Initializer.run do |config|
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
 
   # Specify gems that this application depends on and have them installed with rake gems:install
-  config.gem "rdiscount", :version => "~> 1.3.5", :source => "http://gemcutter.org"
-  config.gem "haml", :version => "~> 2.2.12", :source => "http://gemcutter.org"
-  config.gem "compass", :version => "~> 0.8.17", :source => "http://gemcutter.org"
-  config.gem "will_paginate", :version => "~> 2.3.11", :source => "http://gemcutter.org"
+  config.gem "rdiscount", :version => ">= 1.3.5", :source => "http://gemcutter.org"
+  config.gem "haml", :version => ">= 2.2.12", :source => "http://gemcutter.org"
+  config.gem "compass", :version => ">= 0.8.17", :source => "http://gemcutter.org"
+  config.gem "will_paginate", :version => ">= 2.3.11", :source => "http://gemcutter.org"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named

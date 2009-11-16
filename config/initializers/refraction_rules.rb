447d0024944f8b548544a83e62a1571c6f3fcd0e
Sam Soffes <sam@samsoff.es>
2009-11-15 23:28:56 -0600

    Updated refraction rules

--- config/initializers/refraction_rules.rb
+++ config/initializers/refraction_rules.rb
@@ -1,5 +1,12 @@
 Refraction.configure do |req|
 
+  preferred_host = "new.samsoff.es"
+
+  # Rewrite all of the aliases to the preferred host
+  if req.host != preferred_host
+    req.permanent! "#{req.scheme}://#{preferred_host}#{req.path}"
+  end
+
   # /post/:permalink -> /posts/:permalink
   if req.path =~ /post\/(.*)/
     req.permanent! "/posts/#{$1}"

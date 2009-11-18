0a805d2393649b928caedd19aaef1eb2ad86a33e
Sam Soffes <sam@samsoff.es>
2009-11-17 19:53:29 -0600

    Updated rewrites

--- config/initializers/refraction_rules.rb
+++ config/initializers/refraction_rules.rb
@@ -1,20 +1,36 @@
 Refraction.configure do |req|
 
   preferred_host = "new.samsoff.es"
+  development_host = "samsoffes.local"
 
-  # Rewrite all of the aliases to the preferred host
-  if req.host != preferred_host
+  # Rewrite all of the aliases to the preferred host unless on development
+  if req.host != preferred_host && req.host != development_host && req.host != "localhost"
     req.permanent! "#{req.scheme}://#{preferred_host}#{req.path}"
   end
 
-  # /post/:permalink -> /posts/:permalink
+  # Post permalink
   if req.path =~ /post\/(.*)/
     req.permanent! "/posts/#{$1}"
   end
   
-  # /home -> /
+  # Home
   if req.path == "/home"
     req.permanent! "/"
+  end
+  
+  # About
+  if req.path == "/about.html" || req.path =~ /\/contact(?:\.html)?/
+    req.permanent! "/about"
+  end
+  
+  # Music
+  if req.path == "/music.html"
+    req.permanent! "/music"
+  end
+  
+  # Podcast
+  if req.path == "/the-experiment"
+    req.permanent! "http://itunes.apple.com/WebObjects/MZStore.woa/wa/viewPodcast?id=292395055"
   end
   
 end

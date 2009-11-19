921fc193027834659d5bbc9f587ed844830bd1e0
Sam Soffes <sam@samsoff.es>
2009-11-18 22:18:58 -0600

    Updated less gem config

--- config/environment.rb
+++ config/environment.rb
@@ -24,6 +24,7 @@ Rails::Initializer.run do |config|
   config.gem "rdiscount", :version => ">= 1.3.5", :source => "http://gemcutter.org"
   config.gem "will_paginate", :version => ">= 2.3.11", :source => "http://gemcutter.org"
   config.gem "refraction", :version => ">= 0.1.0", :source => "http://gemcutter.org"
+  config.gem "less", :version => ">= 1.2.12", :source => "http://gemcutter.org"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named

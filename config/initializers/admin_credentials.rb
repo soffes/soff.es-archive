5ad34db35e04caafe0bdc232c33a90402fba2fd4
Sam Soffes <sam@samsoff.es>
2009-12-17 00:23:37 -0600

    Updated admin credentials

--- config/initializers/admin_credentials.rb
+++ config/initializers/admin_credentials.rb
@@ -1,2 +1,4 @@
-ADMIN_USERNAME = "admin" unless defined?(ADMIN_USERNAME)
-ADMIN_PASSWORD = "password" unless defined?(ADMIN_PASSWORD)
+unless Rails.env == "production"
+  ADMIN_USERNAME = "admin" unless defined?(ADMIN_USERNAME)
+  ADMIN_PASSWORD = "password" unless defined?(ADMIN_PASSWORD)
+end

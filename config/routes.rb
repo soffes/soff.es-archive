ce58a99057586eab9ce34e67ee3557e8812ceae6
Sam Soffes <sam@samsoff.es>
2010-02-17 22:43:16 -0600

    Started laying out with HTML5 tags

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 SamSoffes::Application.routes.draw do |map|
+  match "/", :to => "home#index", :as => "root"
+  match "/blog", :to => "home#index", :as => "blog"
+  match "/music", :to => "home#index", :as => "music"
+  match "/about", :to => "home#index", :as => "about"
   resources :posts
 
 end

36e7af3d6f9b8bc3ad303dd998880d8936445ce3
Sam Soffes <sam@samsoff.es>
2010-03-02 00:22:04 -0600

    Cleaned up a bit and finalized markup

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,7 +1,7 @@
 class AboutController < ApplicationController
   
   def index
-    @albums = weekly_album_chart #CACHE["weekly_album_chart"] or weekly_album_chart
+    @albums = CACHE["weekly_album_chart"] or weekly_album_chart
   end
   
   protected
@@ -15,27 +15,29 @@ class AboutController < ApplicationController
     top_albums_url = "http://ws.audioscrobbler.com/2.0/?method=user.getweeklyalbumchart&user=#{LAST_FM_USERNAME}&api_key=#{LAST_FM_API_KEY}"
     albums_xml = REXML::Document.new(Net::HTTP.get_response(URI.parse(top_albums_url)).body)
     albums_xml.elements.each('lfm/weeklyalbumchart/album') do |album|
-      if albums.length < 4
+      if albums.length < 8
         
-        mbid = album.elements['mbid'].text
+        album_name = album.elements['name'].text
+        artist_name = album.elements['artist'].text
         image_url = nil
         
-        if mbid
-          album_art_url = "http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=#{LAST_FM_API_KEY}&mbid=#{mbid}"
+        if album_name
+          album_art_url = "http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=#{LAST_FM_API_KEY}&artist=#{CGI.escape(artist_name)}&album=#{CGI.escape(album_name)}"
+          
           album_art_xml = REXML::Document.new(Net::HTTP.get_response(URI.parse(album_art_url)).body)
-          image_url = album_art_xml.elements['lfm/album/image[@size="large"]'].text
+          image_url = album_art_xml.elements['lfm/album/image[@size="medium"]'].text
         end
         
         Rails.logger.info album_art_url
         albums << {
-          :name => album.elements['name'].text,
+          :name => album_name,
           :image_url => image_url,
-          :artist_name => album.elements['artist'].text,
+          :artist_name => artist_name,
           :url => album.elements['url'].text
         }
       end
     end
-    CACHE.set("weekly_album_chart", albums, 7.days)
+    CACHE.set("weekly_album_chart", albums, 1.day)
     albums
   end
 end

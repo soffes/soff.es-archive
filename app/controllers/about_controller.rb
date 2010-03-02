e77897ad8772ad8c7b996f5be5fb6c58c60d04aa
Sam Soffes <sam@samsoff.es>
2010-03-02 16:23:37 -0600

    Moved stuff into rake tasks

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,43 +1,7 @@
 class AboutController < ApplicationController
   
   def index
-    @albums = CACHE["weekly_album_chart"] or weekly_album_chart
+    @albums = CACHE["lastfm_weekly_album_chart"]
   end
-  
-  protected
-  
-  def weekly_album_chart
-    
-    require 'net/http'
-    require 'rexml/document'
-    
-    albums = []
-    top_albums_url = "http://ws.audioscrobbler.com/2.0/?method=user.getweeklyalbumchart&user=#{LAST_FM_USERNAME}&api_key=#{LAST_FM_API_KEY}"
-    albums_xml = REXML::Document.new(Net::HTTP.get_response(URI.parse(top_albums_url)).body)
-    albums_xml.elements.each('lfm/weeklyalbumchart/album') do |album|
-      if albums.length < 8
-        
-        album_name = album.elements['name'].text
-        artist_name = album.elements['artist'].text
-        image_url = nil
-        
-        if album_name
-          album_art_url = "http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=#{LAST_FM_API_KEY}&artist=#{CGI.escape(artist_name)}&album=#{CGI.escape(album_name)}"
-          
-          album_art_xml = REXML::Document.new(Net::HTTP.get_response(URI.parse(album_art_url)).body)
-          image_url = album_art_xml.elements['lfm/album/image[@size="medium"]'].text
-        end
-        
-        Rails.logger.info album_art_url
-        albums << {
-          :name => album_name,
-          :image_url => image_url,
-          :artist_name => artist_name,
-          :url => album.elements['url'].text
-        }
-      end
-    end
-    CACHE.set("weekly_album_chart", albums, 1.day)
-    albums
-  end
+
 end
---
+++ lib/tasks/cron.rake
@@ -0,0 +1,1 @@
+task :cron => [:"lastfm:update"]

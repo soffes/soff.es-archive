8eaf85e8fe07d09a58ea12f25e41e2d200e96c6b
Sam Soffes <sam@samsoff.es>
2010-03-09 15:57:39 -0600

    Rewrite samsoffes.com URLs

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ require 'sam_soffes/render_directly'
 SamSoffes::Application.routes.draw do |map|
   extend GenericActions::Render
   
+  # Rewrite samsoffes.com URLs
+  # This could be cleaner, but it works for now
+  constraints :host => /(?:www\.)?samsoffes\.com/ do
+    match "/:path" => redirect { |params| "http://samsoff.es/#{params[:path]}" }
+  end
+  
   # Blog
   match "/" => "posts#index", :as => "root"
   resources :posts, :only => [:index, :show]

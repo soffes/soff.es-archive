e59477d7b36ab3f8c31181badb3cf5ab75214b99
Sam Soffes <sam@samsoff.es>
2010-08-01 10:17:24 -0500

    Cleaned up routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ require "not_preferred_host"
 
 SamSoffes::Application.routes.draw do |map|
   
-  # Rewrite no preferred hosts
+  # Rewrite non-preferred hosts in production
   constraints(NotPreferredHost) do
-    match "/:path" => redirect { |params| "http://#{PREFERRED_HOST}/#{params[:path]}" }
+    match "/:path" => redirect { |params| "http://#{NotPreferredHost::PREFERRED_HOST}/#{params[:path]}" }
   end
   
   # Root
-  match "/" => "home#index", :as => "root"
+  root :to => "home#index"
       
   # Blog
   match "/posts.:format" => "posts#index"
@@ -23,6 +23,7 @@ SamSoffes::Application.routes.draw do |map|
   match "/talks" => "pages#talks", :as => "talks"
   match "/orange" => "pages#orange", :as => "orange"
   match "/hello-internet" => "pages#hello_internet", :as => "hello_internet"
+  match "/open-source" => "pages#open_source", :as => "open_source"
   
   # Redirects
   match "/posts" => redirect { |params| "/blog" }
@@ -31,20 +32,19 @@ SamSoffes::Application.routes.draw do |map|
   match "/music.:format" => redirect { |params| "/music" }
   match "/about.:format" => redirect { |params| "/about" }
   match "/mobilex" => redirect { |params| "/talks" }
-  match "/tags/hello-internet" => redirect { |params| "/hello-internet" }
-  match "/tags/hellointernet" => redirect { |params| "/hello-internet" }
-  match "/hellointernet" => redirect { |params| "/hello-internet" }
-  match "/resume" => redirect { |params| "http://assets.samsoff.es/pdf/Sam%20Soffes%20Resume.pdf" }, :as => "resume"
+  match "/:hellointernet" => redirect { |params| "/hello-internet" }, :constraints => { :hellointernet => /hello_?internet/ }
+  match "/resume(.:format)" => redirect { |params| "http://assets.samsoff.es/pdf/Sam%20Soffes%20Resume.pdf" }, :as => "resume"
+  # match "/:opensource" => redirect { |params| "/open-source" }, :constraints => { :opensource => /open[\-_]?source(?:[\-_]?projects)?/ }
   
   # Awesome source redirects
   match "/source(/:code_path)" => redirect { |params|
     code_path = params[:code_path] ? "/tree/master/#{params[:code_path]}" : ""
     "http://github.com/samsoffes/samsoff.es#{code_path}"
-  }, :constraints => { :code_path => /[a-zA-Z0-9_\.\/]+/ }
+  }, :constraints => { :code_path => /[a-zA-Z0-9_\-\.\/]+/ }
   
   # Admin
   namespace :admin do
-    match "/" => "admin#index", :as => "admin"
+    root :to => "admin#index", :as => "admin"
     resources :posts
     resources :tags
   end

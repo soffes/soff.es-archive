7679b2fc68991446e1999f1d9cde744bc532c43f
Sam Soffes <sam@samsoff.es>
2010-09-02 09:29:15 -0500

    * Updated gems * Started converting " or ' because I'm a hardcore Ruby now (heh) * Added follow links in music section on homepage

--- Gemfile
+++ Gemfile
@@ -1,15 +1,15 @@
 source "http://rubygems.org"
 
-gem "rails", "3.0.0.rc"
+gem "rails", "3.0.0"
 gem "pg", "0.9.0"
 gem "rdiscount", "1.6.5"
 gem "will_paginate", "3.0.pre2"
-gem "compass", "0.10.4"
+gem "compass", "0.10.5"
 gem "memcache-client", "1.8.5"
 gem "memcached-northscale", "0.19.5.3", :require => "memcached"
-gem "hoptoad_notifier", "2.3.5"
-gem "tweet-button"
-gem "cloudapp_api"
+gem "hoptoad_notifier", "2.3.6"
+gem "tweet-button", "0.1.0"
+gem "cloudapp_api", "0.0.2"
 
 group :development do
   gem "mongrel", "1.2.0.pre2"
--- config/routes.rb
+++ config/routes.rb
@@ -1,51 +1,51 @@
-require "not_preferred_host"
+require 'not_preferred_host'
 
 SamSoffes::Application.routes.draw do |map|
   
   # Rewrite non-preferred hosts in production
   constraints(NotPreferredHost) do
-    match "/:path" => redirect { |params| "http://#{NotPreferredHost::PREFERRED_HOST}/#{params[:path]}" }
+    match '/:path' => redirect { |params| "http://#{NotPreferredHost::PREFERRED_HOST}/#{params[:path]}" }
   end
   
   # Root
-  root :to => "pages#home"
+  root :to => 'pages#home'
       
   # Blog
-  match "/posts.:format" => "posts#index"
-  match "/blog" => "posts#index", :as => "blog"
+  match '/posts.:format' => 'posts#index'
+  match '/blog' => 'posts#index', :as => 'blog'
   resources :posts, :only => [:show]
   resources :tags, :only => [:index, :show]
   
   # Static pages
-  match "/about" => "pages#about", :as => "about"
-  match "/music" => "pages#music", :as => "music"
-  match "/clearance" => "pages#clearance", :as => "clearance"
-  match "/talks" => "pages#talks", :as => "talks"
-  match "/orange" => "pages#orange", :as => "orange"
-  match "/hello-internet" => "pages#hello_internet", :as => "hello_internet"
-  match "/open-source" => "pages#open_source", :as => "open_source"
+  match '/about' => 'pages#about', :as => 'about'
+  match '/music' => 'pages#music', :as => 'music'
+  match '/clearance' => 'pages#clearance', :as => 'clearance'
+  match '/talks' => 'pages#talks', :as => 'talks'
+  match '/orange' => 'pages#orange', :as => 'orange'
+  match '/hello-internet' => 'pages#hello_internet', :as => 'hello_internet'
+  match '/open-source' => 'pages#open_source', :as => 'open_source'
   
   # Redirects
-  match "/posts" => redirect { |params| "/blog" }
-  match "/post/:permalink" => redirect { |params| "/posts/#{params[:permalink]}" }
-  match "/archive.:format" => redirect { |params| "/blog" }
-  match "/music.:format" => redirect { |params| "/music" }
-  match "/about.:format" => redirect { |params| "/about" }
-  match "/mobilex" => redirect { |params| "/talks" }
-  match "/:hellointernet" => redirect { |params| "/hello-internet" }, :constraints => { :hellointernet => /hello_?internet/ }
-  match "/resume(.:format)" => redirect { |params| "http://assets.samsoff.es/pdf/Sam%20Soffes%20Resume.pdf" }, :as => "resume"
-  # match "/:opensource" => redirect { |params| "/open-source" }, :constraints => { :opensource => /open[\-_]?source(?:[\-_]?projects)?/ }
-  match "/ping" => redirect { |params| "http://c.itunes.apple.com/WebObjects/MZConnections.woa/wa/viewProfile?userId=93483474" }
+  match '/posts' => redirect { |params| '/blog' }
+  match '/post/:permalink' => redirect { |params| "/posts/#{params[:permalink]}" }
+  match '/archive.:format' => redirect { |params| '/blog' }
+  match '/music.:format' => redirect { |params| '/music' }
+  match '/about.:format' => redirect { |params| '/about' }
+  match '/mobilex' => redirect { |params| '/talks' }
+  match '/:hellointernet' => redirect { |params| '/hello-internet' }, :constraints => { :hellointernet => /hello_?internet/ }
+  match '/resume(.:format)' => redirect { |params| 'http://assets.samsoff.es/pdf/Sam%20Soffes%20Resume.pdf' }, :as => 'resume'
+  # match '/:opensource' => redirect { |params| '/open-source' }, :constraints => { :opensource => /open[\-_]?source(?:[\-_]?projects)?/ }
+  match '/ping' => redirect { |params| PING_PROFILE_URL }
   
   # Awesome source redirects
-  match "/source(/:code_path)" => redirect { |params|
-    code_path = params[:code_path] ? "/tree/master/#{params[:code_path]}" : ""
+  match '/source(/:code_path)' => redirect { |params|
+    code_path = params[:code_path] ? "/tree/master/#{params[:code_path]}" : ''
     "http://github.com/samsoffes/samsoff.es#{code_path}"
   }, :constraints => { :code_path => /[a-zA-Z0-9_\-\.\/]+/ }
   
   # Admin
   namespace :admin do
-    root :to => "admin#index", :as => "admin"
+    root :to => 'admin#index', :as => 'admin'
     resources :posts
     resources :tags
   end

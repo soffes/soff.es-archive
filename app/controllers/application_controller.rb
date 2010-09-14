bb8b4fca13469df179cfdba3364fd4ba6851e719
Sam Soffes <sam@samsoff.es>
2010-09-14 01:08:44 -0500

    Updated errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,14 @@ class ApplicationController < ActionController::Base
     user_agent = request.env['HTTP_USER_AGENT']
     user_agent[/iPhone/] && !user_agent[/iPad/]
   end
+  
+  def render_optional_error_file(status_code)
+    known_codes = %w{404 422 500}
+    status = interpret_status(status_code)
+    if known_codes.include?(status)
+      render :template => "errors/#{status[0,3]}", :status => status
+    else
+      render :template => 'errors/unknown', :status => status
+    end
+  end
 end

db4dc003edcb9c73374690b694416b34fe09b9ff
Sam Soffes <sam@samsoff.es>
2011-09-10 12:55:11 -0700

    Got things runnin on Rails 3.1

--- Gemfile
+++ Gemfile
@@ -9,7 +9,11 @@ gem 'goalie', '0.0.4'
 gem 'dalli', '1.1.2'
 
 
-gem 'compass', '0.11.5'
-gem 'coffee-script', '2.2.0'
-gem 'uglifier', '0.5.4'
-gem 'jquery-rails', '1.0.9'
+group :assets do
+  gem 'compass', '0.12.alpha'
+  gem 'sass-rails', '3.1.0'
+  gem 'coffee-rails', '3.1.0'
+  gem 'uglifier'
+end
+
+gem 'jquery-rails'
--- config/application.rb
+++ config/application.rb
@@ -1,8 +1,13 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-Bundler.require :default, Rails.env
-require 'goalie/rails'
+
+if defined?(Bundler)
+  # If you precompile assets before deploying to production, use this line
+  Bundler.require *Rails.groups(:assets => %w(development test))
+  # If you want your assets lazily compiled in production, use this line
+  # Bundler.require(:default, :assets, Rails.env)
+end
 
 module SamSoffes
   class Application < Rails::Application
@@ -25,5 +30,8 @@ module SamSoffes
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    
+    # Version of your assets, change this if you want to expire all your assets
+    config.assets.version = '1.0'
   end
 end

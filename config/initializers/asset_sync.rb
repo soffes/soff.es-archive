22cb413eed14b8dc8fac9cf6eb06591d7557f6fb
Sam Soffes <sam@samsoff.es>
2011-10-30 14:42:18 -0700

    Removed useless files

--- config/initializers/asset_sync.rb
+++
@@ -1,6 +0,0 @@
-AssetSync.configure do |config|
-  config.aws_access_key = ENV['AWS_ACCESS_KEY']
-  config.aws_access_secret = ENV['AWS_ACCESS_SECRET']
-  config.aws_bucket = ENV['AWS_BUCKET']
-  config.existing_remote_files = 'delete'
-end

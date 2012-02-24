d9f1c92f28916bd4a71eb7d7f8675f4952956482
Sam Soffes <sam@samsoff.es>
2012-02-23 23:38:05 -0800

    Remove Google Analytics

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -11,11 +11,6 @@ module LayoutHelper
     content_for(:head) { javascript_include_tag(*args) }
   end
 
-  def google_analytics account = GOOGLE_ANALYTICS_KEY, always = false
-    # This helper should be called at end of the <head> tag
-    "<script>var _gaq=_gaq || [];_gaq.push(['_setAccount','#{account}']);_gaq.push(['_trackPageview']);(function(){var ga=document.createElement('script');ga.async=true;ga.src=('https:'==document.location.protocol?'https://ssl':'http://www')+'.google-analytics.com/ga.js';(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(ga);})();</script>".html_safe if (always || Rails.env.to_sym == :production)
-  end
-
   def like_button(url)
     "<iframe src=\"http://www.facebook.com/plugins/like.php?href=#{CGI::escape(url)}&amp;layout=button_count&amp;show_faces=false&amp;width=450&amp;action=like&amp;colorscheme=light&amp;height=21\" scrolling=\"no\" frameborder=\"0\" id=\"facebook-like-button\" style=\"border:none; overflow:hidden; height:21px;\" allowTransparency=\"true\"></iframe>".html_safe
   end

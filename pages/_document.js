607a94237fd27f1d1354aa74ade3eef347414395
Sam Soffes <sam@soff.es>
2019-02-11 18:35:28 -0800

    Upgrade to Next 8

--- pages/_document.js
+++ pages/_document.js
@@ -1,49 +1,24 @@
-import Document, { Head, Main, NextScript } from 'next/document'
+import Document from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
-  render () {
-    // Hack to prevent unstyled flash with styled-components.
-    // From https://github.com/zeit/next.js/blob/master/examples/with-styled-components/pages/_document.js
+  static async getInitialProps (ctx) {
     const sheet = new ServerStyleSheet()
-    const main = sheet.collectStyles(<Main />)
-    const styleTags = sheet.getStyleElement()
-    return (
-      <html>
-        <Head>
-          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-          <title>Sam Soffes</title>
-          <link rel="mask-icon" sizes="any" href="/favicon.svg" color="#f6291d" />
-          <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-          <link rel="apple-touch-icon" href="/apple-touch-icon-192x192.png" sizes="192x192" />
-          <link rel="me" href="https://www.google.com/profiles/samsoffes" />
-          <link rel="author" type="text/plain" href="/humans.txt" />
-          <meta property="fb:admins" content="1203270129" />
-          <meta property="og:site_name" content="Sam Soffes" />
-          <meta property="og:title" content="Sam Soffes" />
-          <meta property="og:type" content="profile" />
-          <meta property="og:locale" content="en_US" />
-          <link rel="canonical" href="https://soff.es" />
-          <meta property="og:url" content="https://soff.es" />
-          <meta property="og:image" content="https://soffes-assets.s3.amazonaws.com/images/Sam-Soffes.jpg" />
-          <meta property="og:image:secure_url" content="https://soffes-assets.s3.amazonaws.com/images/Sam-Soffes.jpg" />
-          <meta property="og:image:width" content="600" />
-          <meta property="og:image:height" content="600" />
-          <meta property="og:image:type" content="image/png" />
-          <meta property="profile:first_name" content="Sam" />
-          <meta property="profile:last_name" content="Soffes" />
-          <meta property="profile:gender" content="male" />
-          <meta property="profile:username" content="soffes" />
-          <link rel="stylesheet" type="text/css" href="/static/base.css" />
-          {styleTags}
-        </Head>
-        <body>
-          <div className='root'>
-            {main}
-          </div>
-          <NextScript />
-        </body>
-      </html>
-    )
+    const originalRenderPage = ctx.renderPage
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
+        })
+
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: <>{initialProps.styles}{sheet.getStyleElement()}</>
+      }
+    } finally {
+      sheet.seal()
+    }
   }
 }

466110313de590d0aeb74db1e8c1a1c428b83c5c
Sam Soffes <sam@soff.es>
2020-02-21 07:17:13 -0800

    Update year

--- components/footer.js
+++ components/footer.js
@@ -19,7 +19,7 @@ const FooterContainer = styled.footer`
 const Footer = (props) => {
   return(
     <FooterContainer>
-      <p>© 2019 <a href="/" rel="home">Sam Soffes</a></p>
+      <p>© 2020 <a href="/" rel="home">Sam Soffes</a></p>
     </FooterContainer>
   )
 }
